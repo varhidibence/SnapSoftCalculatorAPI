1c7ed75c5448ef4d24534e183949dc828c818684
Várhidi Bence <47637990+varhidibence@users.noreply.github.com>
2022-08-23 18:16:26 +0200

    Calculation model added

---
+++ SnapSoftCalculatorAPI/Models/Calculation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SnapSoftCalculatorAPI.Models
+{
+  public class Calculation
+  {
+
+    /// <summary>
+    /// List of integers for the calculation
+    /// </summary>
+    public List<int> Input { get; set; }
+
+    /// <summary>
+    /// Result of the calculation
+    /// </summary>
+    public List<int> Output { get; set; }
+
+    /// <summary>
+    /// Comment for the calculation
+    /// </summary>
+    public string Comment { get; set; }
+
+    /// <summary>
+    /// Timestamp of the call
+    /// </summary>
+    public DateTime TimestampOfCall { get; set; }
+  }
+}
