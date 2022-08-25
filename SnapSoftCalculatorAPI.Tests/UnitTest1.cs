c199e9839ffbfb0b27fdadf52e3aab8bc7bd4831
Várhidi Bence <47637990+varhidibence@users.noreply.github.com>
2022-08-25 15:59:37 +0200

    Added one unit test, + reference

--- SnapSoftCalculatorAPI.Tests/UnitTest1.cs
+++ SnapSoftCalculatorAPI.Tests/UnitTest1.cs
@@ -1,13 +1,24 @@
+using SnapSoftCalculatorAPI.Services;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace SnapSoftCalculatorAPI.Tests
 {
-  public class UnitTest1
+  public class SnapSoftCalculationService_Test
   {
     [Fact]
-    public void Test1()
+    public void SnapSoftCalculationService_Test4Elements()
     {
+      // Arrange
+      SnapSoftCalculationService service = new SnapSoftCalculationService();
+      List<int> inputNumbers = new List<int> { 1, 2, 3, 4 };
+
+      // Act
+      List<int> output = service.CalculateMagicProduct(inputNumbers);
+
+      // Assert
+      Assert.Equal(inputNumbers.Count, output.Count);
 
     }
   }
